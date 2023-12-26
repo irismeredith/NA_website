54055aa36d38dbb88edeac2af742dd34e770a149
Iris Meredith <79294007+irismeredith@users.noreply.github.com>
2023-12-27 12:32:06 +1300

    Update forms.py
    Added comments

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,9 @@
+# This file contains the contact form class for the website.
+
+# Author: Iris Meredith
+
+# Last modified: 27/12/2023
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email, email_validator
