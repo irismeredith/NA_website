0f65c910e23bfaecd03e7cd088fd0028b3a97bd3
Iris Meredith <79294007+irismeredith@users.noreply.github.com>
2023-12-27 12:31:07 +1300

    Update __init__.py
    Added comments

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,9 @@
+# This is a basic Flask app initiation: the only really interesting thing here is probably the use of whitenoise for serving static files
+
+# Author: Iris Meredith
+
+# Last modified: 27/12/2023
+
 from flask import Flask
 from flask_mail import Mail
 from app import config
